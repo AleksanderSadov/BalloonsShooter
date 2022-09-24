3b0bfb388da3b6f758d07bb5237438b3b4632d5b
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-09-24 16:01:13 +0700

    Working on game over scene - in progress

--- Assets/Code/Scripts/Gameplay/Managers/GameManager.cs
+++ Assets/Code/Scripts/Gameplay/Managers/GameManager.cs
@@ -1,19 +1,37 @@
 using BalloonsShooter.Gameplay.Events;
 using BalloonsShooter.Gameplay.Managers;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace BalloonsShooter.Gameplay.Manager
 {
     public class GameManager : MonoBehaviour
     {
+        public string gameOverSceneName;
+
         private void Awake()
         {
             EventsManager.Broadcast(new GameStartedEvent());
         }
 
+        private void OnEnable()
+        {
+            EventsManager.AddListener<PlayerDeathEvent>(OnPlayerDeath);
+        }
+
+        private void OnDisable()
+        {
+            EventsManager.RemoveListener<PlayerDeathEvent>(OnPlayerDeath);
+        }
+
         private void OnDestroy()
         {
             EventsManager.Broadcast(new GameEndedEvent());
+        }
+
+        private void OnPlayerDeath(PlayerDeathEvent evt)
+        {
+            SceneManager.LoadScene(gameOverSceneName, LoadSceneMode.Single);
         }
     }
 }

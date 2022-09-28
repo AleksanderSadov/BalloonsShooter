3748ff4fe213e6bd3afff200d031b06c0550f654
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-09-28 19:32:57 +0700

    Add explosion sound

--- Assets/Code/Scripts/Effects/Explosion.cs
+++ Assets/Code/Scripts/Effects/Explosion.cs
@@ -7,9 +7,26 @@ namespace BalloonsShooter.Effects
     public class Explosion : MonoBehaviour
     {
         public GameObject parent;
+        public float randomPitchStep;
+
+        private AudioSource explosionSound;
+        private float originalPitch;
+
+        private void Awake()
+        {
+            explosionSound = GetComponent<AudioSource>();
+            originalPitch = explosionSound.pitch;
+        }
+
+        public void ExplosionStarted()
+        {
+            explosionSound.pitch = originalPitch + Random.Range(originalPitch - randomPitchStep, originalPitch + randomPitchStep);
+            explosionSound.Play();
+        }
 
         public void ExplosionEnded()
         {
+            explosionSound.Stop();
             EventsManager.Broadcast(new ExplosionEndedEvent(parent));
         }
     }
